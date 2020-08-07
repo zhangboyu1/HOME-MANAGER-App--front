3195900c227d13525e8366e75da0c6eefcd60469
sadsada <boyu@yahoo.com>
2020-08-07 13:16:06 +1000

    kkk

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from "axios";
-import './LogIn.css';
+import './Login.css';
 import { auth } from '../Store/Auth';
 import { Link } from 'react-router-dom';
 import { checkInputValidity } from '../Store/Inputvalidity'
